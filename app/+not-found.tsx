b29e0c290f2361dab32caa161e22083b18612c6d
Sajal Batra <sajalbatra7@gmail.com>
2024-11-21 14:37:37 +0530

    first commit

---
+++ app/+not-found.tsx
@@ -0,0 +1,11 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+    </>
+  );
+}
